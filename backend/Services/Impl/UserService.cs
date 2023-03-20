5db8cd516bedabc4317e4fd18ed9fd6f16216c55
Apollostowel <apollostowel@icloud.com>
2023-03-20 12:23:41 +0200

    Building Authentication and Authorization.

--- backend/Services/Impl/UserService.cs
+++ backend/Services/Impl/UserService.cs
@@ -15,12 +15,57 @@ public class UserService : IUserService
         _jwTokenService = jWTokenService;
     }
 
-    public async Task<LoginReponseDTO?> LoginAsync(LoginDTO request)
+    public async Task<LoginReponseDTO?> LoginAsync(CredentialsDTO request)
     {
         var user = await _userManager.FindByEmailAsync(request.Email);
 
-        
+        if (user is null)
+        {
+            return null;
+        }
 
+        if (!await _userManager.CheckPasswordAsync(user, request.Password))
+        {
+            return null;
+        }
+
+        return await _jwTokenService.GenerateTokenAsync(user);
+    }
+
+    public async Task<User?> RegisterAsync(UserRegistrationDTO request)
+    {
+        var user = new User
+        {
+            UserName = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (!result.Succeeded)
+        {
+            return null;
+        }
+
+        return user;
+    }
+
+    public async Task<bool> UserRolesAsync(string?[] roles, User user)
+    {
+        if (roles is null)
+        {
+            throw new ArgumentNullException(nameof(roles));
+        }
+        
+        var nonNullRoles = roles.Where(r => r != null).Select(r => r!);
+        var result = await _userManager.AddToRolesAsync(user, nonNullRoles);
+
+        if (!result.Succeeded)
+        {
+            return false;
+        }
+        return result.Succeeded;
     }
 
 
