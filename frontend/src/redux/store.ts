e9358e06269ca0c8c12156387799b704ccf62d33
Apollostowel <apollostowel@icloud.com>
2023-04-29 01:06:35 +0300

    Added reducers to the store.

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,8 +1,23 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import bookReducer from './reducers/bookReducer';
+import userReducer from './reducers/userReducer';
+import authorReducer from './reducers/authorReducer';
+import categoryReducer from './reducers/categoryReducer';
+import publisherReducer from './reducers/publisherReducer';
+import copiesReducer from './reducers/copiesReducer';
+import loanReducer from './reducers/loanReducer';
+import notificationReducer from './reducers/notificationReducer';
 
 export const store = configureStore({
   reducer: {
-   
+    user: userReducer,
+    book: bookReducer,
+    author: authorReducer,
+    category: categoryReducer,
+    publisher: publisherReducer,
+    copy: copiesReducer,
+    loan: loanReducer,
+    notifcation: notificationReducer
   },
 });
 
