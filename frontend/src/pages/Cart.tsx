675cd1b5ade41e381b34d04108ed6bf58457e6d5
Apollostowel <apollostowel@icloud.com>
2023-05-04 13:26:07 +0300

    Implemented Cart page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,6 +1,44 @@
-import { Box } from "@mui/material"
+import { useEffect, useState } from "react";
+import {
+    Box,
+    Button,
+    Divider,
+    Grid,
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks"
+import { addToCart, removeFromCart, emptyCart } from "../redux/reducers/cartReducer";
 
 const Cart = () => {
+    const cart = useAppSelector(state => state.cart)
+    const dispatch = useAppDispatch()
+    const [show, setShow] = useState(false)
+
+    const total = cart.reduce((a, b) => a + b.amount, 0)
+
+    const handleShow = () => {
+        cart.forEach(item => {
+            if (item.amount > 0) {
+                setShow(true)
+            }
+        })
+    }
+
+    useEffect(() => {
+        handleShow()
+    })
+
     return (
         <Box style={{
             display: "flex",
@@ -9,9 +47,60 @@ const Cart = () => {
             marginTop: "15px"
         }}
         >
-            Cart
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item md={8}>
+                    <Typography textAlign="center">Checkout</Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Book</TableCell>
+                                    <TableCell align="center">Publisher</TableCell>
+                                    <TableCell align="center">Quantity</TableCell>
+                                    <TableCell align="center">Edit Items</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {cart.map(item =>
+                                    <TableRow key={item.id}>
+                                        <TableCell align="left">{item.title}</TableCell>
+                                        <TableCell align="center">{item.publisher.publisherName.toString()}</TableCell>
+                                        <TableCell align="center">{item.amount}</TableCell>
+                                        <TableCell align="center">
+                                            <IconButton onClick={() => dispatch(addToCart(item))}>
+                                                <AddIcon fontSize="small" />
+                                            </IconButton>
+                                            <IconButton onClick={() => dispatch(removeFromCart(item))}>
+                                                <RemoveIcon fontSize="small" />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell />
+                                    <TableCell />
+                                    <TableCell align="center">{total} items</TableCell>
+                                    <TableCell align="center">
+                                        {show ?
+                                            <Button color="success">Place order</Button>
+                                            : null
+                                        }
+                                    </TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                    </TableContainer>
+                    <Divider variant="middle" />
+                </Grid>
+            </Grid>
+            {show ?
+                <Button color="warning" onClick={() => dispatch(emptyCart())}>Empty Cart</Button>
+                : null
+            }
         </Box>
     )
 }
 
-export default Cart;
+export default Cart
