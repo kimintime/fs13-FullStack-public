ea6776fb220507421b0466b07af682d9256e2ee9
Apollostowel <apollostowel@icloud.com>
2023-05-06 15:57:32 +0300

    tracking cart bugs

--- frontend/src/redux/reducers/cartReducer.ts
+++ frontend/src/redux/reducers/cartReducer.ts
@@ -1,42 +1,32 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CartItem } from "../../types/cart"
 
-const initialState: CartItem[] = []
+const initialCartItems = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems') as string)
+    : [];
+
+const initialState: CartItem[] = initialCartItems;
 
 const cartSlice = createSlice({
     name: "cartSlice",
     initialState: initialState,
     reducers: {
-        addToCart: (state, action: PayloadAction<CartItem>) => {
-            let isInCart = false
-            let cartAmount = 1
-
-            state.forEach(item => {
-                if (item.id === action.payload.id) {
-                    isInCart = true
-                    cartAmount += item.amount
-                }
-
-                return item
-            })
-
-            const newItem: CartItem = {...action.payload, amount: cartAmount }
-            
-            if (isInCart) {
-                return (
-                    state.map(item => 
-                        item.id === action.payload.id ? newItem : item))
-
+        addToCart: (state, action) => {
+            const { id } = action.payload;
+            const existingItem = state.find(item => item.id === id);
+            if (existingItem) {
+              existingItem.amount += 1;
+              localStorage.setItem('cartItems', JSON.stringify(state));
             } else {
-
-                return [...state, newItem]
+              state.push({ ...action.payload, amount: 1 });
+              localStorage.setItem('cartItems', JSON.stringify(state));
             }
-        },
+          },
         removeFromCart: (state, action: PayloadAction<CartItem>) => {
             let cartAmount = 0
 
             state.forEach(item => {
-                if ( item.id === action.payload.id) {
+                if (item.id === action.payload.id) {
                     cartAmount = item.amount
                 }
             })
@@ -46,21 +36,24 @@ const cartSlice = createSlice({
 
             } else {
 
-                let newItem: CartItem = {...action.payload, amount: cartAmount - 1 }
+                let newItem: CartItem = { ...action.payload, amount: cartAmount - 1 };
 
-                return (
-                    state.map(item => 
-                        item.id === action.payload.id ? newItem : item)
-                )
+                const newState = state.map(item =>
+                    item.id === action.payload.id ? newItem : item
+                );
+
+                localStorage.setItem('cartItems', JSON.stringify(newState));
+                return newState;
             }
         },
         emptyCart: (state) => {
             state.length = 0
+            localStorage.removeItem('cartItems')
         },
     }
 
 })
 
 const cartReducer = cartSlice.reducer
-export const {addToCart, removeFromCart, emptyCart} = cartSlice.actions
+export const { addToCart, removeFromCart, emptyCart } = cartSlice.actions
 export default cartReducer
